acb6ffbf4540bbf4c011b3668176543291a162fc
mattwalsh13 <math.walsh13@gmail.com>
2026-07-15 19:45:31 -0400

    Add unfinished preprocessing for basic operations

--- preprocess.py
+++ preprocess.py
@@ -1,14 +1,43 @@
 import sys
+import re
 
 def main(pre_file: str, processed_file: str) -> None:
     """
     Takes a normal .c file that uses int_enc, and injects the correct function call in place of the operation., also ensure encrypted values stay hidden.
     """
-    with open(pre_file, 'r', encoding='utf-8') as file:
+    lines: list[str] = []
+
+    with open(pre_file, "r", encoding="utf-8") as file:
         for line in file:
-            if line.startswith('//'):
+            if line.startswith("//"):
                 continue
-            print(line.strip())
+            lines.append(line)
+
+    variables: set[str] = set()
+    ops = ["+", "-"]
+    op_strs = ["add", "sub"]
+    op_str_lookup = dict(zip(ops, op_strs))
+
+    for i, unstripped_line in enumerate(lines):
+        line = unstripped_line.strip()
+        # Variable declaration
+        if line.startswith("int_enc"):
+            variables.add(line.split("=")[0].split(" ")[1].rstrip(";"))
+        # Variable used
+        elif any(word in line for word in variables):
+            # Operation done
+            if any(op in line for op in ops):
+                op = next(op for op in ops if op in line)
+                leading_whitespace = re.match(r"^\s*", unstripped_line).group(0)
+                tokens = line.strip().split("=")
+                dest = tokens[0].strip(" ")
+                operand_1 = tokens[1].split(op)[0].strip(" ")
+                operand_2 = tokens[1].split(op)[1].rstrip(";").strip(" ")
+                lines[i] = f"{leading_whitespace}{dest} = enc_{op_str_lookup[op]}({operand_1}, {operand_2});\n"
+
+    with open(processed_file, "w") as file:
+        for line in lines:
+            file.write(line)
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
