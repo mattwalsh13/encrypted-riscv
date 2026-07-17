4f2c0a8f30f913f8de579054320dcf7338797699
mattwalsh13 <math.walsh13@gmail.com>
2026-07-17 18:27:33 -0400

    N/A

--- main.pp.c
+++ main.pp.c
@@ -2,25 +2,23 @@
 
 int main() {
     int_enc list[5] = { {1}, {2}, {3} };
-    int_enc = x = 0xD;
-    int_enc = y = 0x7;
+    int_enc x = 0xD;
+    int_enc y = 0x7;
     int_enc result;
 
-    result = = x & y;
-    result = = x + y;
+    result = x & y;
+    result = x + y;
 
-    result = = addi_enc ( x , 13 );
+    result = addi_enc(x, 13);
 
-    int_enc = a = x + y , b = y - x;
+    int_enc a = x + y, b = y - x;
 
     x += y;
-
-    result = = addDouble ( x , y );
 
     return 0;
 }
 
 int addDouble(int_enc a, int_enc b) {
-    int_enc = c = a + b;
+    int_enc c = a + b;
     return c + b;
 }
